bdd478460e0150b8586ebbdeefbf6b276d8bc2ba
Gifer <giferpleasure@gmail.com>
2026-08-31 15:16:23 +0300

    Замена ссылки на слово NOW

--- components/Header.tsx
+++ components/Header.tsx
@@ -44,7 +44,7 @@ export default function Header() {
               rel="noopener noreferrer"
               className="text-accent hover:text-accent/80 font-medium transition"
             >
-              Fantastic
+              Now
             </Link>
           </div>
         </div>
