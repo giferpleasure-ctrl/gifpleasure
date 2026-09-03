61cfd9584fca36937d17749fb12b6ba448994168
Gifer <giferpleasure@gmail.com>
2026-09-03 08:27:24 +0300

    Добавлено 250 новых гифок 03.09.26 part 1

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -17,7 +17,7 @@ const s3 = new S3Client({
 });
 
 const BUCKET = process.env.SELECTEL_BUCKET || "gifpleasure-storage";
-const IMGBB_API_KEY = "9586f00e7b05ccfc67dd2f3fa7c14acb";
+const IMGBB_API_KEY = "f5e8a4a4fa77f7898cd3b0a553cf09af";
 const IMGBB_UPLOAD_URL = "https://api.imgbb.com/1/upload";
 const LOCAL_BACKUP_DIR = "H:/gifpleasure_archive/gifs";
 // ==================================
