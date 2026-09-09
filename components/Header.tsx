b3f22136ba3696d72a90b1e525684dfbb4bdafc8
Gifer <giferpleasure@gmail.com>
2026-09-09 12:17:06 +0300

    Добавлено 256 новых гифок 09.09.26

--- components/Header.tsx
+++ components/Header.tsx
@@ -44,7 +44,7 @@ export default function Header() {
               rel="noopener noreferrer"
               className="text-accent hover:text-accent/80 font-medium transition"
             >
-              FULL
+              YOU WIN
             </Link>
           </div>
         </div>
