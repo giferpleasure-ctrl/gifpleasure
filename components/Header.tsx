1bb62a12bec4c61d7d79710a76e5ad8d062c0497
Gifer <giferpleasure@gmail.com>
2026-09-10 10:02:58 +0300

    Добавлено Angel Rivas новых гифок 10.09.26

--- components/Header.tsx
+++ components/Header.tsx
@@ -44,7 +44,7 @@ export default function Header() {
               rel="noopener noreferrer"
               className="text-accent hover:text-accent/80 font-medium transition"
             >
-              YOU WIN
+              Click
             </Link>
           </div>
         </div>
